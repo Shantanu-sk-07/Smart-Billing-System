80babcdf9da7ad9170b1089655d953c82f345632
Shantanu5787 <shantanu_gk.championxi@outlook.com>
2026-05-25 19:48:02 +0530

    updated

---
+++ SiddhiVinayak_ganesh_mandal/Backend/src/main/java/com/suraj/MurtiSystem/MurtiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.MurtiSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurtiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurtiSystemApplication.class, args);
+	}
+
+}
---
+++ SiddhiVinayak_ganesh_mandal/Backend/src/test/java/com/suraj/MurtiSystem/MurtiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.MurtiSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurtiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
